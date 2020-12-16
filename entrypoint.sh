0a5af49290f10308eab444a2d6d0650e413d4ea9
samarthdd <samarth@smalldaytech.com>
2020-12-16 21:16:44 +0530

    updation

--- entrypoint.sh
+++ entrypoint.sh
@@ -56,9 +56,9 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 debug "Enumerating contents of $1"
 STRING="Creating_OVA"
 printf 'Enumerating contents of' "$1"
-for folder in $(find $1 -maxdepth 1 -execdir basename '{}' ';'); do
+for folder in $(find $1 -maxdepth 0 -execdir basename '{}' ';'); do
   printf '%s\n' "$folder"
-for file in $(find "$1/$folder" -maxdepth 2 -type f -name '*.md' -execdir basename '{}' ';'); do
+for file in $(find "$1/$folder" -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';'); do
     if [[ "$file" == *"$STRING"* ]];then
     printf '%s\n' "$file"
     else
