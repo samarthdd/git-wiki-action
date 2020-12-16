b9c9b23cece9e4ee758b69d261ee83e10bcb8916
samarthdd <samarth@smalldaytech.com>
2020-12-16 12:58:10 +0530

    updation

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,7 @@ GIT_REPOSITORY_URL="https://${GH_PERSONAL_ACCESS_TOKEN}@github.com/$GITHUB_REPOS
 debug "Checking out wiki repository"
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 (
-    #cd "$tmp_dir" || exit 1
+    cd "$tmp_dir" || exit 1
     git init
     git config user.name "$GITHUB_ACTOR"
     git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
@@ -58,12 +58,12 @@ for file in $(find $1 -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';
     #cd "$tmp_dir" || exit 1
     cat "$1/$file" >> wiki.md
     #cd ..
-    #cp "$1/$file" "$tmp_dir"
+    cp wiki.md "$tmp_dir"
 done
 
 debug "Committing and pushing changes"
 (
-    #cd "$tmp_dir" || exit 1
+    cd "$tmp_dir" || exit 1
     git add .
     git commit -m "$WIKI_COMMIT_MESSAGE"
     git push --set-upstream "$GIT_REPOSITORY_URL" master
