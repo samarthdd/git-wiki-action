10c779d426d01f459b8e29808c21edecf05b5d2f
samarthdd <samarth@smalldaytech.com>
2020-12-16 12:34:57 +0530

    updation

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,14 +53,17 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 ) || exit 1
 
 debug "Enumerating contents of $1"
+cat > "$tmp_dir"/wiki.txt
 for file in $(find $1 -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';'); do
     debug "Copying $file"
-    cp "$1/$file" "$tmp_dir"
+    cat "$1/$file" >> "$tmp_dir"/wiki.txt
+    #cp "$1/$file" "$tmp_dir"
 done
 
 debug "Committing and pushing changes"
 (
     cd "$tmp_dir" || exit 1
+
     git add .
     git commit -m "$WIKI_COMMIT_MESSAGE"
     git push --set-upstream "$GIT_REPOSITORY_URL" master
